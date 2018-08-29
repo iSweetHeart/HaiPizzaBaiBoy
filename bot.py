cee7405dd4df266cf3c4f8cff0c8ece26310de10
iSweetHeart <42302258+iSweetHeart@users.noreply.github.com>
2018-08-30 00:02:27 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from discord.voice_client import VoiceClient
+
+
+startup_extensions = ["Music"]
+bot = commands.Bot("z!")
+
+@bot.event
+async def on_ready():
+   print("Bot Opend")
+
+class Main_Commands():
+        def __init__(self, bot):
+         self.bot = bot 
+   
+@bot.command(pass_context=True) 
+async def hello(ctx):
+    await bot.say("hi :wave:")
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '(): ()'.format(type(e).__name__. e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+ 
+bot.run("NDg0NDU0NDE2ODA0NjEwMDYy.DmiWhg.uzEwApfIZTiFo1A0kn0wfkqg9-k")
